7656bbd47d332b476b69dce93764b93196ce1fe4
Diogo Vianna <diogoviannaaraujo@gmail.com>
2014-10-21 21:19:26 -0400

    created initial base structure

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-estufa
+Projeto Estufa
 ======
+
+Controlador de estufa hidroponica
+
+Funcionamento
+------
+
+TO-DO: Preencher sobre como funciona importacao de dados e coisas do genero
---
+++ db.php
@@ -0,0 +1,6 @@
+// AQUI IREMOS IMPLEMENTAR UMA CLASSE DE ABSTRACAO DO BANCO
+// 1o Passo - Criar a classe
+// 2o Passo - Setar Variaveis de configuracao
+// 3o Passo - Criar função constructor que conecta no banco
+// 4o Passo - Criar funcao que abstrai a execucao de um query
+// Obs.: Provavelmente ja tem uma dessa pronta na internet
