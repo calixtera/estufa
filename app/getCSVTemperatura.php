dc95eb857d0ea7099cd5d262288eadf943a77d33
Diogo Vianna <diogoviannaaraujo@gmail.com>
2014-10-27 23:02:42 -0400

    test update

--- app/getCSVTemperatura.php
+++ app/getCSVTemperatura.php
@@ -4,9 +4,9 @@
 	
 	$result = mysql_query("SELECT * FROM temperatura");
 	
-	echo('id, data, valor');
+	echo("data, valor\r\n");
 	while($row = mysql_fetch_array($result)) {
-	    echo($row["idTemperatura"] . "," . $row["data/hora"] . "," . $row["valorTemperatura"]);
+	    echo($row["data/hora"] . "," . $row["valorTemperatura"]) . "\r\n";
 	}
 	
 ?>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert('wtf?')
+var chart = c3.generate({
+	bindto: '#temperatura',
+    data: {
+	    x: 'data',
+        url: '/app/getCSVTemperatura.php',
+        mimeType: 'csv'
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                format: '%Y-%m-%d %H:%M:%S'
+            }
+        }
+    }
+});
