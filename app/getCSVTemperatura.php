1ab98a41735fcb0c161fd8005265ad67d6f460b4
Diogo Vianna <diogoviannaaraujo@gmail.com>
2014-10-27 23:11:59 -0400

    temperature is ready

--- app/getCSVTemperatura.php
+++ app/getCSVTemperatura.php
@@ -2,11 +2,13 @@
 
 	include 'db.php';
 	
+	header("Content-Type: text/plain");
+	
 	$result = mysql_query("SELECT * FROM temperatura");
 	
 	echo("data, valor\r\n");
 	while($row = mysql_fetch_array($result)) {
-	    echo($row["data/hora"] . "," . $row["valorTemperatura"]) . "\r\n";
+	    echo($row["data/hora"] . ", " . $row["valorTemperatura"]) . "\r\n";
 	}
 	
 ?>
--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var chart = c3.generate({
 	bindto: '#temperatura',
     data: {
 	    x: 'data',
+	    xFormat: '%Y-%m-%d %H:%M:%S',
         url: '/app/getCSVTemperatura.php',
         mimeType: 'csv'
     },
