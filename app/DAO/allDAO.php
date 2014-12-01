73fe056cd47549e097f1bc0703bdb2e5a58f3638
Guilherme Calixto <calixtao@gmail.com>
2014-12-01 11:03:17 -0200

    Criação allDAO

---
+++ app/DAO/allDAO.php
@@ -0,0 +1,17 @@
+<?php
+include "umidadeDAO.php";
+include "vazaoDAO.php";
+include "phDAO.php";
+include "energiaDAO.php";
+include "iluminacaoDAO.php";
+include "temperaturaDAO.php";
+include "condutividadeDAO.php";
+
+umidade();
+vazao();
+ph();
+energia();
+iluminacao();
+temperatura();
+condutividade();
+?>
