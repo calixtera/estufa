3d285656321ae8606d0a1d281a0ba15ae3fdf318
Diogo Vianna <diogoviannaaraujo@gmail.com>
2014-12-01 07:22:31 -0500

    adding dao

---
+++ app/DAO/condutividadeDAO.php
@@ -0,0 +1,69 @@
+
+<?php
+
+function condutividade (){
+//include 'C:\xampp\htdocs\EstufaAutomatizada\sms\human_gateway_client_api\HumanClientMain.php';
+
+//$humanMultipleSend = new HumanMultipleSend("jadsvicto.api", "3Uyv28FADt");
+//$tipo = HumanMultipleSend::TYPE_C;
+//$msg_list  = "556181236581;teste0;004"."\n";
+
+
+//$responses = $humanMultipleSend->sendMultipleList($tipo, $msg_list, 0);
+
+$servidor = 'localhost';
+$banco      = 'projeto_estufa';
+$usuario  = 'root'; //alterar
+$senha    = ''; // alterar
+$link     = mysql_connect($servidor, $usuario, $senha);
+$db          = mysql_select_db($banco,$link);
+
+$date=new DateTime(); //this returns the current date time
+$result = $date->format('Y-m-d-H-i-s');
+// Obtem o conteudo do arquivo (arquivo.txt)
+
+$dados = file_get_contents('C:\Users\jads\Desktop\condutividade.txt');
+
+    if ($dados != null){
+        
+        // Quebra o conteudo do arquivo usando o separador \n (quebra de linha)
+$itens = explode("\n", $dados);
+
+// Inicio um array nesta variavel
+ $dado = array();
+
+// Intrução inicial do MySQL. Será unida com o resto da consulta no foreach() abaixo
+$sql  = "INSERT INTO `condutividade` (`idCondutividade`, `valorCondutividade`, `data/hora`) VALUES";
+
+// Quebro cada item que chega do array $itens, para serem insidos no MySQL
+foreach ( $itens as $item ) {
+    $dados = explode(';', $item);
+ 
+    // Como os campos nome e data tinham um espaço antes, usei a função trim() em cada dado para retirar esses espaços
+    foreach ( $dados as $dadoTmp ) {
+        $dado = trim($dadoTmp);
+    }
+
+    $sql .= " ('', '".$dado."', '$result'),";
+}
+
+// Retira a virgula que sobrou no final da consulta
+$sql = substr($sql, 0, -1);
+
+// Executa a query, inserindo os dados.
+$resultado = mysql_query($sql) or die(mysql_error());
+$responses = $humanMultipleSend->sendMultipleList($tipo, $msg_list, 0);
+    }
+        
+    else{
+        echo ("arquivo nao encontrado");
+    }
+mysql_close();
+
+//if ($dados<0 )//arrumar
+//{
+//$responses = $humanMultipleSend->sendMultipleList($tipo, $msg_list, 0);
+//}
+}
+?>
+
---
+++ app/DAO/energiaDAO.php
@@ -0,0 +1,66 @@
+<?php
+
+function energia (){
+$servidor = 'localhost';
+$banco      = 'projeto_estufa';
+$usuario  = 'root'; //alterar
+$senha    = ''; //alterar
+$link     = mysql_connect($servidor, $usuario, $senha);
+$db          = mysql_select_db($banco,$link);
+
+
+ //include '../sms/human_gateway_client_api/HumanClientMain.php';
+//$humanMultipleSend = new HumanMultipleSend("jadsvicto.api", "3Uyv28FADt");
+//$tipo = HumanMultipleSend::TYPE_C;
+//$msg_list  = "556181236581;teste0;004"."\n";
+
+$date=new DateTime(); //this returns the current date time
+$result = $date->format('Y-m-d-H-i-s');
+// Obtem o conteudo do arquivo (arquivo.txt)
+
+$dados = file_get_contents('C:\Users\jads\Desktop\condutividade.txt'); 
+//$dados = file_get_contents('energia.txt');
+
+    if ($dados != null){
+        
+        // Quebra o conteudo do arquivo usando o separador \n (quebra de linha)
+$itens = explode("\n", $dados);
+
+// Inicio um array nesta variavel
+ $dado = array();
+
+// Intrução inicial do MySQL. Será unida com o resto da consulta no foreach() abaixo
+$sql  = "INSERT INTO `energia` (`idEnergia`, `valorEnergia`, `data/hora`) VALUES";
+
+// Quebro cada item que chega do array $itens, para serem insidos no MySQL
+foreach ( $itens as $item ) {
+    $dados = explode(';', $item);
+ 
+    // Como os campos nome e data tinham um espaço antes, usei a função trim() em cada dado para retirar esses espaços
+    foreach ( $dados as $dadoTmp ) {
+        $dado = trim($dadoTmp);
+    }
+
+    $sql .= " ('', '".$dado."', '$result'),";
+}
+
+// Retira a virgula que sobrou no final da consulta
+$sql = substr($sql, 0, -1);
+
+// Executa a query, inserindo os dados.
+$resultado = mysql_query($sql) or die(mysql_error());
+
+    }
+        
+    else{
+        echo ("arquivo nao encontrado");
+    }
+mysql_close();
+}
+//if ($dados<0 )//arrumar
+//{
+//$responses = $humanMultipleSend->sendMultipleList($tipo, $msg_list, 0);
+//}
+
+?>
+
